91610e9a5742b978383eb7626566c4f52e7e0c6a
Arseny <senika-2010@mail.ru>
2021-12-06 20:10:11 +1000

    add theory activity

--- app/src/main/java/ru/netology/pet_app/MainActivity.kt
+++ app/src/main/java/ru/netology/pet_app/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.netology.pet_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    var btnActTwo: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun version(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Версия 0.1", Toast.LENGTH_SHORT)
+        myToast.show()
+
+    }
+
+    fun theoryActivity(view: View) {
+        val theory = Intent(this, TheoryActivity::class.java)
+        startActivity(theory)
     }
 }
