7a24a6d6b5c1da5b28769697938ab6f37b1b74b1
Arseny <senika-2010@mail.ru>
2021-12-09 23:09:19 +1000

    add content for theory

---
+++ app/src/main/java/ru/netology/pet_app/OperationsWithNumbers.kt
@@ -0,0 +1,11 @@
+package ru.netology.pet_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OperationsWithNumbers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operations_with_numbers)
+    }
+}
