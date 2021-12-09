ea1d5d8bdeb5e971b90ee52eebf15619a3a5ef13
Arseny <senika-2010@mail.ru>
2021-12-09 23:13:04 +1000

    minor fixes

--- app/src/main/java/ru/netology/pet_app/TheoryActivity.kt
+++ app/src/main/java/ru/netology/pet_app/TheoryActivity.kt
@@ -1,11 +1,21 @@
 package ru.netology.pet_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.TextView
 
 class TheoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_theory)
+
+    }
+
+    fun operationWithNumbersActivity(view: View) {
+        val operationWithNumbers = Intent(this, OperationsWithNumbers::class.java)
+        startActivity(operationWithNumbers)
     }
 }
