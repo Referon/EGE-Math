5b5714991db5ef4eb817a425ea0412802fd52688
Arseny <senika-2010@mail.ru>
2022-01-12 19:40:40 +1000

    added new test and updated for UI in test

---
+++ app/src/main/java/ru/netology/pet_app/TestP1.kt
@@ -0,0 +1,11 @@
+package ru.netology.pet_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestP1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_p1)
+    }
+}
--- app/src/main/java/ru/netology/pet_app/TestsActivity.kt
+++ app/src/main/java/ru/netology/pet_app/TestsActivity.kt
@@ -1,11 +1,18 @@
 package ru.netology.pet_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TestsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tests)
+    }
+
+    fun operationOne(view: View) {
+        val operationOne = Intent(this, TestP1::class.java)
+        startActivity(operationOne)
     }
 }
