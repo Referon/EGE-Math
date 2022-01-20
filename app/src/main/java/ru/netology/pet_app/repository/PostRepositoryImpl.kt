19588b3810460cbfced5a8309f8ef731c0e8ee9f
Arseny <senika-2010@mail.ru>
2022-01-20 11:21:18 +1000

    updated PostRepositoryImpl & PostViewModel

---
+++ app/src/main/java/ru/netology/pet_app/repository/PostRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package ru.netology.pet_app.repository
+
+import ru.netology.pet_app.dao.TaskDao
+
+
+class PostRepositoryImpl(private val dao: TaskDao) : PostRepository {
+/* p1... = p - профильный уровень, 1 - номер задачи*/
+
+
+    override fun getMessageById(id: Int): String? {
+        return dao.getMessageById(id).toString()
+    }
+
+    override fun getId(): Int {
+        return dao.getId()
+    }
+
+    override fun setDecidedByID(id: Int) {
+        dao.setDecidedById(id)
+    }
+
+    override fun getSize(): Int {
+        return dao.getSize()
+    }
+
+    override fun getAnswerById(id: Int): String {
+        return dao.getAnswerById(id).toString()
+    }
+
+}
--- app/src/main/java/ru/netology/pet_app/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/pet_app/viewmodel/PostViewModel.kt
@@ -1,7 +1,18 @@
 package ru.netology.pet_app.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import ru.netology.pet_app.db.AppDb
+import ru.netology.pet_app.repository.PostRepository
+import ru.netology.pet_app.repository.PostRepositoryImpl
 
-class PostViewModel: ViewModel() {
+class PostViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryImpl(AppDb.getInstance(application).taskDao())
 
+    fun getMessageById(id: Int): String? = repository.getMessageById(id)
+    fun getAnswerById(id: Int): String = repository.getAnswerById(id)
+    fun getId(): Int = repository.getId()
+    fun getSize() = repository.getSize()
+    fun setDecidedById(id: Int) = repository.setDecidedByID(id)
 }
