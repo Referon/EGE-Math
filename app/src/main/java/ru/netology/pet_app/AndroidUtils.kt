412cf45c58ae5ecac19a31e067db7f6024a97976
Arseny <senika-2010@mail.ru>
2022-01-30 14:44:41 +1000

    minor fixes & object for hide keyboard

---
+++ app/src/main/java/ru/netology/pet_app/AndroidUtils.kt
@@ -0,0 +1,13 @@
+package ru.netology.pet_app
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+object AndroidUtils {
+
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
