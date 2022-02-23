567b580ea1df1b41ae0335f508ac9ce592e58115
Arseny <senika-2010@mail.ru>
2022-02-24 08:01:14 +1000

    minor updates

--- app/src/main/java/ru/netology/pet_app/variants/MainVariant.kt
+++ app/src/main/java/ru/netology/pet_app/variants/MainVariant.kt
@@ -1,28 +1,29 @@
-package ru.netology.pet_app
+package ru.netology.pet_app.variants
 
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
+import android.text.method.ScrollingMovementMethod
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import ru.netology.pet_app.ResultVariantActivity
 import ru.netology.pet_app.data.YorTaskVariant
-import ru.netology.pet_app.databinding.ActivityVariant1Binding
+import ru.netology.pet_app.databinding.MainVariantsLayoutBinding
 import ru.netology.pet_app.viewmodel.PostViewModel
-import java.io.File
+
+abstract class MainVariant(open val type: Int): AppCompatActivity() {
+   open val viewModel: PostViewModel by viewModels()
 
 
-class Variant1Activity : AppCompatActivity() {
-    val viewModel: PostViewModel by viewModels()
-    val type = 1
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        val binding = ActivityVariant1Binding.inflate(layoutInflater)
+        val binding = MainVariantsLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val intent: Intent = getIntent()
-
 
         var task: YorTaskVariant
 
@@ -31,21 +32,26 @@ class Variant1Activity : AppCompatActivity() {
         val answer = viewModel.getAnswerVariantById(id)
         val picture = viewModel.getPictureVariantById(id)
 
+        binding.messgae.setMovementMethod(ScrollingMovementMethod())
 
-       binding.messgae.text = message
+        binding.messgae.text = message
 
-        val parsePicture = Uri.parse("android.resource://ru.netology.pet_app/drawable/$picture")
 
-        Glide
-            .with(this)
-            .load(parsePicture)
-            .into(binding.image)
+            val parsePicture = Uri.parse("android.resource://ru.netology.pet_app/drawable/$picture")
+
+
+            Glide
+                .with(this)
+                .load(parsePicture)
+                .apply(RequestOptions().override(800, 800))
+                .into(binding.image)
+
 
         binding.next.setOnClickListener {
 
             viewModel.saveTaskVariant(YorTaskVariant(id,type,message,answer,binding.answer.text.toString(),picture,1))
             viewModel.setDecidedVariantById(id)
-            if (viewModel.sizeSaveTask() == 2) {
+            if (viewModel.sizeSaveTask() == 11) {
                 val intentResult: Intent = Intent(this, ResultVariantActivity::class.java)
                 viewModel.removeDecidedVariant(type)
                 finish()
@@ -62,5 +68,4 @@ class Variant1Activity : AppCompatActivity() {
         viewModel.removeDecidedVariant(type)
         super.onBackPressed()
     }
-
 }
